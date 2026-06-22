23d85e1b3f6ee058f5998f0fd514db517e4f494c
palzurma <mazurerap@gmail.com>
2026-06-22 12:47:49 +0200

    feature/Proyecto_piedra_papel_tijera

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(palabra, veces) {
+if (veces < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < veces; i++) {
+    string += palabra;
+  }
+  return string;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
